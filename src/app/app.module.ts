6538b05abab6a03452d4906110c80124723a8a9b
Tomasz <tplocic20@gmail.com>
2019-01-24 23:08:08 +0100

    New version changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,12 +47,12 @@ import { SearchProvider } from '../providers/search';
 
 
 var fireBaseConfig = {
-  apiKey: "AIzaSyDlCSFFdJ2kdm8-2ZkKPZasVWHqc0bfotg",
-  authDomain: "przepisnik-v2.firebaseapp.com",
-  databaseURL: "https://przepisnik-v2.firebaseio.com",
-  projectId: "przepisnik-v2",
-  storageBucket: "przepisnik-v2.appspot.com",
-  messagingSenderId: "529215752695"
+  apiKey: "AIzaSyAIHYAHC6ykzoTnO25vEpxwkXm9_fS_Jeo",
+  authDomain: "przepisnik.firebaseapp.com",
+  databaseURL: "https://przepisnik.firebaseio.com",
+  projectId: "firebase-przepisnik",
+  storageBucket: "firebase-przepisnik.appspot.com",
+  messagingSenderId: "457833981759"
 };
 
 @NgModule({
