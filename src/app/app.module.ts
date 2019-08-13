625ed3c3563560ebbc166a8a9175ffbcdcdff690
Tomasz Płocic <tomasz.plocic@neti-soft.com>
2019-08-13 05:43:41 +0200

    Test config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,12 +47,13 @@ import { SearchProvider } from '../providers/search';
 
 
 var fireBaseConfig = {
-  apiKey: "AIzaSyAIHYAHC6ykzoTnO25vEpxwkXm9_fS_Jeo",
-  authDomain: "przepisnik.firebaseapp.com",
-  databaseURL: "https://przepisnik.firebaseio.com",
-  projectId: "firebase-przepisnik",
-  storageBucket: "firebase-przepisnik.appspot.com",
-  messagingSenderId: "457833981759"
+  apiKey: "AIzaSyDlCSFFdJ2kdm8-2ZkKPZasVWHqc0bfotg",
+  authDomain: "przepisnik-v2.firebaseapp.com",
+  databaseURL: "https://przepisnik-v2.firebaseio.com",
+  projectId: "przepisnik-v2",
+  storageBucket: "przepisnik-v2.appspot.com",
+  messagingSenderId: "529215752695",
+  appId: "1:529215752695:web:18d237eacca082cc"
 };
 
 @NgModule({
